44f9d0494002a19e085f72fee86e0347ab07dd0f
aadimarwah <aadimarwah316@gmail.com>
2024-08-07 00:51:02 +0530

    Update PrimeNumber.cpp

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -1,28 +1,25 @@
 /*Using This code we will check wheter a number is prime or not*/
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
-    int num1, i;
-    cout << "Enter a number here to check:";
-    cin >> num1;
-    if (num1 == 0 || num1 == 1)
-    {
-        cout << "\n Prime Number";
-        
-    }
-    for (i = 2; i < num1; i++)
-    {
-
-        if (num1 % i == 0)
-        {
-
-            cout << "\n Not Prime";
+int main(){
+    int num1,i,flag;
+    cout<<"\nEnter number here to check if a number is prime or not: ";
+    cin>>num1;
+    for(i=2;i<num1;i++){
+        flag=1;
+        if(num1%i==0){
+            flag=0;
             break;
         }
-        else
-        {
-            cout << "\n Prime Number";
+        
+    }
+    for(i=2;i<num1;i++){
+        if(flag==1){
+            cout<<"\nThe number is prime";
+            break;
+        }
+        else{
+            cout<<"\nThe number is not prime";
             break;
         }
     }
